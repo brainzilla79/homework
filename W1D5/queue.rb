c5d53bb4ce01207fc028a715e6b7beb98d226700
Alex <alexanderhapp@me.com>
2017-08-03 22:32:37 -0700

    Complete queue class

---
+++ W1D5/queue.rb
@@ -0,0 +1,19 @@
+class Queue
+
+  def initialize
+    @store = []
+  end
+
+  def enqueue(el)
+    @store.unshift(el)
+  end
+
+  def dequeue
+    @store.pop
+  end
+
+  def show
+    @store.dup
+  end
+
+end
