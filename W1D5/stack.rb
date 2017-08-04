8ccd003d550a12316070382b97c567a9c0e0e668
Alex <alexanderhapp@me.com>
2017-08-03 22:23:21 -0700

    Complete stack class

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -1,15 +1,19 @@
 class Stack
   def initialize
-
+    @store = []
   end
 
   def add(el)
+    @store << el
+    el
   end
 
-  def remove(el)
+  def remove
+    @store.pop
   end
 
   def show
-  end 
+    @store.dup
+  end
 
 end
