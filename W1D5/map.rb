a7bf40898be61d7ce4be29b1563832f0849bcae1
Alex <alexanderhapp@me.com>
2017-08-03 23:18:24 -0700

    Complete map class

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -7,9 +7,27 @@ class Map
     @store.each do |entry|
       if entry[0] == key
         entry[1] = value
-        return [key, value]
+        return value
       end
     end
     @store << [key, value]
+    value
+  end
+
+  def lookup(key)
+    @store.each do |entry|
+      return entry[1] if entry[0] == key
+    end
+    nil
+  end
+
+  def remove(key)
+    value = self.lookup(key)
+    @store = @store.reject { |entry| entry[0] == key }
+    value
+  end
+
+  def show
+    @store.dup
   end
 end
