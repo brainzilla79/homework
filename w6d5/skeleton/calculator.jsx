352b231e2371adad6f335e5cefdd8634671d1953
Alex <alexanderhapp@me.com>
2017-09-08 08:09:10 -0700

    Finish add button

--- w6d5/skeleton/calculator.jsx
+++ w6d5/skeleton/calculator.jsx
@@ -4,16 +4,35 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = { num1: "", num2: "", result: 0 };
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+    this.add = this.add.bind(this);
 
-    // your code here
   }
 
-  // your code here
+  setNum1(e) {
+    const num1 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num1 });
+  }
+
+  setNum2(e) {
+    const num2 = e.target.value ? parseInt(e.target.value) : "";
+    this.setState({ num2 });
+  }
+
+  add(e) {
+    e.preventDefault();
+    const result = this.state.num1 + this.state.num2;
+    this.setState({ result });
+  }
 
   render() {
     return (
       <div>
-        <h1>{this.state.result}</h1> // replace this with your code
+        <h1>{this.state.result}</h1>
+          <input onChange={this.setNum1} value={this.state.num1} />
+          <input onChange={this.setNum2} value={this.state.num2} />
+        <button onClick={this.add}>Add</button>
       </div>
     );
   }
